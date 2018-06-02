62e9bd9688086c2399dee4235b9551cbd778b9ef
yinyue200 <yinyue200@outlook.com>
2018-06-02 11:36:28 +0800

    init

--- bugrepo/UnitTest1.cs
+++ bugrepo/UnitTest1.cs
@@ -3,12 +3,20 @@ using Xunit;
 
 namespace bugrepo
 {
+    class Test:Nito.Comparers.EquatableBaseWithOperators<Test>
+    {
+        static Test()
+        {
+            DefaultComparer = Nito.Comparers.EqualityComparerBuilder.For<Test>().Default();
+        }
+    }
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-
+            Test a = null;
+            Assert.True(a == null);
         }
     }
 }
